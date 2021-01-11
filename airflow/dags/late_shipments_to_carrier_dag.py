8f09d6c54dc3b853566632dfb4b618d7a7225e71
Andŕé Felippe <andre.felippemsp@gmail.com>
2021-01-11 08:59:16 -0300

    11/01/2021

--- airflow/dags/late_shipments_to_carrier_dag.py
+++ airflow/dags/late_shipments_to_carrier_dag.py
@@ -15,7 +15,7 @@ s3_download_operator = BashOperator(task_id='s3_download',
 # Executar os comandos Pyspark para retornar as solicitações
 # onde o vendedor perdeu o prazo de entrega a transportadora
 spark_prazo_operator = BashOperator(task_id='spark_missed_deadline_job',
-                                              bash_command='python ../scripts/spark_missed_deadline_job.py',
+                                              bash_command='python ../scripts/spark_prazos_perdidos.py',
                                               dag=dag)
 
 # Especificar que a task acima depende que o download dos dados ocorra
