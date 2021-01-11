43bc4684a8ce40e21bbf5f5de7c36a8822ae3267
Andŕé Felippe <andre.felippemsp@gmail.com>
2021-01-11 09:56:23 -0300

    11/01

--- airflow/scripts/s3_upload.py
+++ airflow/scripts/s3_upload.py
@@ -2,33 +2,31 @@ import os
 from boto3.s3.transfer import S3Transfer
 import boto3
 
-########################################
-## Edit the keys/paths for your setup ##
-########################################
-access_key = 'your_access_key_here'
-secret_key = 'your_secret_key_here'
-s3_bucket_name = 'your_s3_bucket_name'
-s3_filename = 'brazilian-ecommerce.zip'
-s3_filename = 'missed_shipping_limit_orders.csv'
+# Editar as chaves de acesso
+k_acess = 'sua_acess_key'
+k_secret = 'sua_secret_key'
+nome_s3_bucket = 'seu_s3_bucket_name'
+s3_arquivo = 'brazilian-ecommerce.zip'
+s3_arquivo_csv = 'missed_shipping_limit_orders.csv'
 client = boto3.client('s3',
-                      aws_access_key_id = access_key,
-                      aws_secret_access_key = secret_key)
-
-print('client')
+                      aws_access_key_id=k_acess,
+                      aws_secret_access_key=k_secret)
 
 transfer = S3Transfer(client)
 
-print('transfer - ' + s3_bucket_name)
+print('transfer - ' + nome_s3_bucket)
 
-# Define function to scan through the Spark output uploadDirectory,
-# identify csv files, and upload them to the S3 bucket
-def uploadDirectory(filepath, s3_bucket_name):
+
+# Definir função para analisar pelo diretório de saida do Spark.
+# Identifica arquivos csv e faz o upload no S3 bucket
+def enviarDiretorio(filepath, s3_bucket_name):
     for root, dirs, files in os.walk(filepath):
         for file in files:
-            # Transfer only csv files
+            # Transfere apenas arquivos csv
             if file.endswith('csv'):
                 transfer.upload_file(os.path.join(root, file),
                                      s3_bucket_name,
-                                     "Clean_Data/" + file) # File put into Clean-Data folder
+                                     "Clean_Data/" + file)  # Arquivo inserido na pasta clean-data
 
-uploadDirectory(filepath = filepath, s3_bucket_name = s3_bucket_name)
+
+enviarDiretorio(filepath=s3_arquivo_csv, s3_bucket_name=nome_s3_bucket)
